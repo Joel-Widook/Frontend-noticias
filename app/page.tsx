00d1eeaea6b9609badc07c6122eaa87eb9eefc89
Joel Esteban Diaz Arévalo <joelestebandiazarevalo@192.168.0.6>
2025-03-20 17:25:11 -0500

    feat(layout): Maquetación del layout de publicidades y contexto global que controla las publicidades a visualizar creados

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Fragment>
-      <div className="grid grid-cols-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-amber-100">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="grid lg:grid-cols-[9%_80%_9%] lg:gap-[1%] h-auto p-4 sm:p-10 bg-background rounded-[10px] ">
+        <div className="col-span-3 h-[60px] lg:h-[100px] bg-[#FEECEC] rounded-[10px] border-[1px] border-[#FF4F4F]"></div>
+        <div className="hidden lg:block h-[400px] bg-[#FEECEC] rounded-[10px] border-[1px] border-[#FF4F4F]"></div>
+        <div className="col-span-3 lg:col-span-1 flex h-[800px] bg-primary-400"></div>
+        <div className="hidden lg:block h-[400px] bg-[#FEECEC] rounded-[10px] border-[1px] border-[#FF4F4F]"></div>
+        <div className="col-span-3 h-[60px] lg:h-[100px] bg-[#FEECEC] rounded-[10px] border-[1px] border-[#FF4F4F]"></div>
       </div>
+      <div className="fixed bottom-4 right-4 h-[144px] w-[240px] lg:h-[240px] lg:w-[360px] bg-[#FEECEC] rounded-[10px] border-[1px] border-[#FF4F4F]"></div>
     </Fragment>
   );
 }
